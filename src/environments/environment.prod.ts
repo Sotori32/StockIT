46ce0ee8671389b206eb875e7ab7bb9d1d6f1022
Sotori32 <37298537+Sotori32@users.noreply.github.com>
2022-11-05 08:19:47 +0100

    Init

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
+  firebase: {
+    projectId: 'stock-it-6d205',
+    appId: '1:74263609237:web:80500b4584ffe0ac79c09c',
+    storageBucket: 'stock-it-6d205.appspot.com',
+    apiKey: 'AIzaSyB8FHHJFooX-_x-kidk-1hGscQrdUo84mI',
+    authDomain: 'stock-it-6d205.firebaseapp.com',
+    messagingSenderId: '74263609237',
+  },
   production: true
 };
