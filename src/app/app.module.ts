dec65d6a7f9ccf46d6050cafa69c523f4885e989
Sotori32 <37298537+Sotori32@users.noreply.github.com>
2022-11-12 00:46:23 +0100

    Storage component styling

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -18,20 +18,26 @@ import { MatInputModule } from '@angular/material/input';
 import { MatButtonModule } from '@angular/material/button';
 import { MatCardModule } from '@angular/material/card';
 import { MatIconModule } from '@angular/material/icon';
-import {MatTableModule} from '@angular/material/table';
+import { MatTableModule } from '@angular/material/table';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatDividerModule } from '@angular/material/divider';
 
 import { AppComponent } from './app.component';
 import { SignupComponent } from './components/signup/signup.component';
 import { LoginComponent } from './components/login/login.component';
 import { AuthGuard } from './services/auth.guard';
 import { StorageComponent } from './components/storage/storage.component';
+import { HeaderComponent } from './components/header/header.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     SignupComponent,
     LoginComponent,
-    StorageComponent
+    StorageComponent,
+    HeaderComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
@@ -53,6 +59,8 @@ import { StorageComponent } from './components/storage/storage.component';
     MatCardModule,
     MatIconModule,
     MatTableModule,
+    MatToolbarModule,
+    MatDividerModule,
   ],
   providers: [AuthGuard],
   bootstrap: [AppComponent]
